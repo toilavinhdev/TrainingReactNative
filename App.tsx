fee0422fb4e1d4f2cc5a4d47daf1dc82225133e4
toilavinh <hoangdvinh68@gmail.com>
2024-03-19 20:06:10 +0700

    up

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
 import SignInScreen from "./src/screens/auth/SignInScreen";
-import SignUpScreen from "./src/screens/auth/SignUpScreen";
 
 export default function App() {
-  return (
-    <>
-      <SignInScreen></SignInScreen>
-      <SignUpScreen></SignUpScreen>
-    </>
-  );
+  return <SignInScreen></SignInScreen>;
 }
