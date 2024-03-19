a52f019e679b53cf092dd5cc455605ff896e4fcf
toilavinh <hoangdvinh68@gmail.com>
2024-03-19 19:49:38 +0700

    save

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from "./src/screens/auth/SignInScreen";
+import SignUpScreen from "./src/screens/auth/SignUpScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SignInScreen></SignInScreen>
+      <SignUpScreen></SignUpScreen>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
